4a4ba565c016571410cf730f41baa8396f82f177
jsalamanca11 <j.salamanca611@gmail.com>
2016-12-13 16:30:17 -0500

    creating variables

--- liri.js
+++ liri.js
@@ -1,7 +1,17 @@
 var fs = require('fs');
-var Twitter = require('twitter');
+var Twitter = require('./key.js');
 var Spotify = require('spotify');
 //For OMDB movie Api
 var request = require('request');
 
-var action
+//commands to get data from twitter, spotify or movie
+var action = process.argv[2];
+//
+if (action === "my-tweets") {
+	var params = {screen_name: 'jsala109 '};
+	Twitter.get('statuses/user_timeline', params, function(error, tweets, response){
+  	if (!error) {
+    console.log(tweets);
+ 	 }
+})
+}
