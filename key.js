88d51c7b259c4cf470630973d9d6cb82d2a152dc
jsalamanca11 <j.salamanca611@gmail.com>
2016-12-13 15:42:20 -0500

    twitter keys

--- key.js
+++ key.js
@@ -1,8 +1,10 @@
+
+
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '2OrNwrY4Ia0Kk2OQKTWaPEdop',
+  consumer_secret: 'lBCqdzthrlgAqcDNmjE5USivjRmHyvZRSjK1PS2GCX4p3i3TC1',
+  access_token_key: '807225380092477440-kki513vWW5BB4nbpPoQAsT0DmX4PWsD',
+  access_token_secret: 'reKsaspnAgTP5HVGtuyWVxc5MKqqPsM9eUFBC02PERl57',
 }
--- liri.js
+++ liri.js
@@ -1,1 +1,7 @@
-liri.js
+var fs = require('fs');
+var Twitter = require('twitter');
+var Spotify = require('spotify');
+//For OMDB movie Api
+var request = require('request');
+
+var action
